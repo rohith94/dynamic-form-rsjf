85b50581dd86cf08511069c070a6cdc803cadc33
Rohith <rohith.m94@gmail.com>
2021-12-09 16:45:38 +0530

    test

--- src/pages/Start.js
+++ src/pages/Start.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Start = () => {
   return (
-    <div>
+    <div className="ui raised very padded text  container segment">
       <div>
         <a href="/page4" target="_blank">
           Page 4
